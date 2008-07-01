710e1e1ef209395f0f61840c19008a4bd70ef893
steve <steve@lesseverything.com>
2008-07-01 11:09:20 -0400

    cleaning up lib_requires.

--- config/initializers/lib_requires.rb
+++ config/initializers/lib_requires.rb
@@ -1,10 +1,5 @@
 require 'rubygems'
-#require 'uuidtools'
 require 'ostruct'
-#require 'file_column'
-#require 'acts_as_ferret'
-#require 'youtube_g'
-#require 'colored'
 
 
 Less::JsRoutes.generate!
