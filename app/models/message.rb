9ce5c6609b09a863d96c346733ce664e7ee1187a
steve <steve@lesseverything.com>
2008-08-01 09:51:59 -0400

    fixng up assimovt's changes just a bit

--- app/models/message.rb
+++ app/models/message.rb
@@ -17,4 +17,9 @@ class Message < ActiveRecord::Base
   belongs_to :sender, :class_name => "Profile"
   belongs_to :receiver, :class_name => "Profile"
   validates_presence_of :body, :subject, :sender, :receiver
+  
+  
+  def unread?
+    !read
+  end
 end
