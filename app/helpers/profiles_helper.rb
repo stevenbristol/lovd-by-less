3fd8772615b3783c27fbab7e831fc77f99295b04
steve <steve@lesseverything.com>
2009-01-22 19:53:14 -0500

    fixing bug with icons and deleted users

--- app/helpers/profiles_helper.rb
+++ app/helpers/profiles_helper.rb
@@ -6,7 +6,7 @@ module ProfilesHelper
   def icon profile, size = :small, img_opts = {}
     return "" if profile.nil?
     img_opts = {:title => profile.full_name, :alt => profile.full_name, :class => size}.merge(img_opts)
-    link_to(avatar_tag(profile, {:size => size, :file_column_version => size }, img_opts), profile_path(profile))
+    link_to(avatar_tag(profile, {:size => size, :file_column_version => size }, img_opts), profile_path(profile)) rescue ''
   end
   
   def location_link profile = @p
