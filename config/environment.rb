fe39dcc679e9c7db059b8bc560df5bb309accc9e
Jonathon Brenner <jonbrenner@gmail.com>
2009-09-18 18:40:06 -0400

    raked rails:update. removed colored and redgreen due to incompatibility. removed shoulda plugin and added a config.gem for the new shoulda gem

--- config/environment.rb
+++ config/environment.rb
@@ -20,13 +20,12 @@ Rails::Initializer.run do |config|
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
   config.gem 'will_paginate', :version => '~> 2.2.2'
-  config.gem 'colored', :version=> '1.1'
   config.gem 'youtube-g', :version=> '0.4.9.9', :lib=>'youtube_g'
   config.gem 'uuidtools', :version=> '1.0.4'
   config.gem 'hpricot', :version=> '0.6.164'
   config.gem 'mocha', :version=> '0.9.3'
-  config.gem 'redgreen', :version=> '1.2.2' unless ENV['TM_MODE']
   config.gem 'gcnovus-avatar', :version=> '0.0.7', :lib => 'avatar'
+  config.gem "thoughtbot-shoulda", :lib => "shoulda", :source => "http://gems.github.com"
   
   # Only load the plugins named here, in the order given (default is alphabetical).
   # :all can be used as a placeholder for all plugins not explicitly named
